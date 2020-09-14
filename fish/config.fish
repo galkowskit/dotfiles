c27186fc95a4e7ad7ad9282ab6b751425876d948
Tomasz Gałkowski <galkowski.t@gmail.com>
2020-09-14 13:57:18 +0200

    explicitly set nvim as default editor

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,7 @@ export PATH="/Users/tkg/.deno/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$PATH:$GOPATH/bin"
 
+export VISUAL=nvim
+export EDITOR="$VISUAL"
+
 export FZF_DEFAULT_COMMAND='fd --type f'
